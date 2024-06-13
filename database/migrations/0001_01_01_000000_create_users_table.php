8dd22e9c253e466541d1d5a3f6844292c4267218
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-06-13 12:40:34 +0700

    feat: add description column to users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->nullable()->default('Fullstack');
+            $table->string('status')->nullable()->default('Available');
+            $table->string('picture')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
